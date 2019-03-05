62694fab4d941c7b5b48527a40c4d6e839ce801b
Yulia Goryk <gorykyulia@gmail.com>
2019-03-05 19:07:06 +0200

    Fixed verify of numbers

--- 4.03/js/main.js
+++ 4.03/js/main.js
@@ -1,32 +1,43 @@
-var calc = document.getElementById("calc");
+"use strict"
 
+var calc = document.getElementById("calc");
 calc.addEventListener('click', calculate);
 
 function calculate(){
   var radios = document.querySelectorAll('input[type="radio"]');
 
-  var a = +prompt("Enter the first number: ");
-  var b = +prompt("Enter the second number: ");
+  var a = prompt("Enter the first number: ");
+  var b = prompt("Enter the second number: ");
 
-  if(!isNumeric(a) || !isNumeric(b)){
-    alert("Error! Enter the numbers again");
-  } else{
-    for(var i = 0; i < radios.length; i++) {
-      if(radios[i].checked) {
-        switch (radios[i].value) {
-          case 'add' : alert(add(a,b));
-            break;
-          case 'subs' : alert(subs(a,b));
-            break;
-          case 'mult' : alert(mult(a,b));
-            break;
-          case 'div' : alert(div(a,b));
-            break;
-          default: alert("Error");
+  if(!a || !b || a === null || b === null) {
+    alert ("Error! You enter nothing, try again");
+  } else {
+    a = +a;
+    b = +b;
+    if(!isNumeric(a) || !isNumeric(b)){
+      alert("Error! Enter the numbers again");
+    } else{
+      for(var i = 0; i < radios.length; i++) {
+        if(radios[i].checked) {
+          switch (radios[i].value) {
+            case 'add' : alert(add(a,b));
+              break;
+            case 'subs' : alert(subs(a,b));
+              break;
+            case 'mult' : alert(mult(a,b));
+              break;
+            case 'div' : alert(div(a,b));
+              break;
+            default: alert("Error");
+          }
         }
       }
     }
   }
+
+
+
+
 }
 
 function isNumeric(num){
